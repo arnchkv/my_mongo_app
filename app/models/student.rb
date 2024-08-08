388536b91f3d071ad37d009078212756b8b33833
Arnav <arnchkv@gmail.com>
2024-08-08 13:05:37 +0530

    email format checking and indexing done

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,5 +6,11 @@ class Student
 
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
+
+  # Ensure the email field is unique and properly formatted
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # Index on email to ensure uniqueness and improve query performance
+  index({ email: 1 }, { unique: true, background: true })
 end
